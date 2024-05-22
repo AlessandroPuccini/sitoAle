4d4883e36805036655c871e0af3fadbb427a9d59
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-05-23 00:40:14 +0200

    modifiche genrali e creazione navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './Navbar'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+    <>
+    <header id='header'>
+     <Navbar />
+    </header>
+    </>
   
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+  
+    <nav>
+        <div className='logo-name'>ALESSANDRO PUCCINI</div>
+        <div className='menu-items'>
+        <ul>
+            <li><a href='#about'>ABOUT</a></li>
+            <li><a href='#contact'>CONTACT</a></li>
+        </ul>
+        </div>
+    </nav>
+
+  )
+}
+export default Navbar
