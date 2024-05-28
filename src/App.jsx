5db19041a11db4c9e890a5744bb7e541b4920ea1
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-05-29 00:10:02 +0200

    correzzioni bug front page e definizione template con animazioni in css

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,15 @@ function App() {
     </header>
     <main>
       <div className='frontPage'>
+     
+        <div className='circle1'>  <p className='film'>FILM</p> </div>
+      
         
+        <div className='circle2'><p className='dev'>DEV</p></div>
+        
+
+        <div className='circle3'> <p className='stills'>STILLS</p></div>
+       
       </div>
     </main>
     </>
