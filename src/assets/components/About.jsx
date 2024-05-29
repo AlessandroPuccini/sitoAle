31410839f26c8661903fc0524b6fa00acd9b2f90
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-05-29 11:49:32 +0200

    Aggiunta pagina about

---
+++ src/assets/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function About() {
+  return (
+    <>
+      <Navbar />
+        <section id='about'>
+            <div className='about'>
+            <h2>About</h2>
+            <p>
+                I am a filmmaker and developer based in London. I have a passion for
+                creating visual stories that are both engaging and thought-provoking.
+                I have worked on a variety of projects, from short films to
+                documentaries, and I am always looking for new opportunities to
+                collaborate with other creatives.
+            </p>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default About
